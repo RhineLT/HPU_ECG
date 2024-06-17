1afe898120e80dbbe3c1c47bf8923581da921487
RhineLT <69440427+RhineLT@users.noreply.github.com>
2024-06-17 23:38:24 +0800

    Update training_save_deep_models_tf.py

--- training_save_deep_models_tf.py
+++ training_save_deep_models_tf.py
@@ -4,11 +4,23 @@ import tensorflow as tf
 from tensorflow.keras import models, layers, optimizers, losses
 import argparse
 from sklearn.metrics import precision_score, recall_score, f1_score, fbeta_score
-from help_code_demo_tf import ECG_DataSET, ToTensor, create_dataset # Adjust as necessary for compatibility with TensorFlow
-from models.model_tf import AFNet
-from models.model_tf import AFNet_Com
-from models.model_tf import AFNet_light
-# from models.effi import EFFNet
+from help_code_demo_tf import ECG_DataSET, ToTensor, create_dataset
+from models.model_tf import AFNet, AFNet_Com, AFNet_light
+
+# Define TeacherNet and StudentNet
+from models.model_tf import TeacherNet, StudentNet
+
+def distillation_loss(y_true, y_pred_student, y_pred_teacher, temperature=3, alpha=0.5):
+    loss_fn = losses.SparseCategoricalCrossentropy(from_logits=True)
+    # Standard cross entropy loss
+    student_loss = loss_fn(y_true, y_pred_student)
+    
+    # KL divergence loss
+    y_pred_teacher_soft = tf.nn.softmax(y_pred_teacher / temperature)
+    y_pred_student_soft = tf.nn.softmax(y_pred_student / temperature)
+    kl_loss = losses.KLDivergence()(y_pred_teacher_soft, y_pred_student_soft) * (temperature ** 2)
+    
+    return alpha * student_loss + (1 - alpha) * kl_loss
 
 def main():
     # Hyperparameters
@@ -23,20 +35,22 @@ def main():
     Test_loss = []
     Test_acc = []
     Test_fb = []
-    best_fb = 0.0  # Initialize best F-beta score
-    best_acc = 0.0  # Initialize best accuracy
+    best_fb = 0.0
+    best_acc = 0.0
     
-
     physical_devices = tf.config.list_physical_devices('GPU')
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-    # Instantiating NN
-    # net = AFNet()
+    # Instantiate Teacher and Student networks
+    teacher_net = TeacherNet()
+    teacher_net.build((1, 1250, 1, 1))
+    # teacher_net.load_weights('./ECG_student_net_tf.h5')  # Load pretrained weights
+    teacher_net.trainable = False  # Freeze teacher network
 
-    # net = AFNet_Com()
-    net = AFNet_light()
-    net.build((1,1250,1,1))
-    print(net.summary())
+    student_net = StudentNet()
+    student_net.build((1, 1250, 1, 1))
+    print(student_net.summary())
+
     optimizer = optimizers.Adam(learning_rate=LR)
     loss_object = losses.SparseCategoricalCrossentropy(from_logits=True)
 
@@ -55,11 +69,12 @@ def main():
         i = 0
         for step, (x, y) in enumerate(trainloader):
             with tf.GradientTape() as tape:
-                logits = net(x, training=True)
-                loss = loss_object(y, logits)
-                grads = tape.gradient(loss, net.trainable_variables)
-                optimizer.apply_gradients(zip(grads, net.trainable_variables))
-                pred = tf.argmax(logits, axis=1)
+                logits_student = student_net(x, training=True)
+                logits_teacher = teacher_net(x, training=False)
+                loss = distillation_loss(y, logits_student, logits_teacher)
+                grads = tape.gradient(loss, student_net.trainable_variables)
+                optimizer.apply_gradients(zip(grads, student_net.trainable_variables))
+                pred = tf.argmax(logits_student, axis=1)
                 correct += tf.reduce_sum(tf.cast(tf.equal(pred, y), tf.float32))
                 accuracy += correct / x.shape[0]
                 correct = 0.0
@@ -81,9 +96,9 @@ def main():
         i = 0.0
         running_loss_test = 0.0
         for x, y in testloader:
-            logits = net(x, training=False)
-            test_loss = loss_object(y, logits)
-            pred = tf.argmax(logits, axis=1)
+            logits_student = student_net(x, training=False)
+            test_loss = loss_object(y, logits_student)
+            pred = tf.argmax(logits_student, axis=1)
             total += y.shape[0]
             correct += tf.reduce_sum(tf.cast(tf.equal(pred, y), tf.float32))
             running_loss_test += test_loss
@@ -100,17 +115,17 @@ def main():
         Test_fb.append(fb)
         print('F-beta score: %.5f' % fb)
         # Save the model for each epoch
-        net.save(f'./saved_models/{epoch+1}_ECG_net_tf.h5')
+        student_net.save(f'./saved_models/{epoch+1}_ECG_student_net_tf.h5')
 
         # Save the model if the test accuracy is the best we've seen so far
         if test_accuracy > best_acc:
             best_acc = test_accuracy
-            net.save('./saved_models/best_ECG_net_tf.h5')
-        if fb>best_fb:
+            student_net.save('./saved_models/best_ECG_student_net_tf.h5')
+        if fb > best_fb:
             best_fb = fb
-            net.save('./saved_models/fb_ECG_net_tf.h5')
+            student_net.save('./saved_models/fb_ECG_student_net_tf.h5')
     # Save final model and results
-    net.save('./saved_models/ECG_net_tf.h5')
+    student_net.save('./saved_models/ECG_student_net_tf.h5')
 
     # Write results to file
     with open('./saved_models/loss_acc.txt', 'w') as file:
@@ -133,7 +148,7 @@ if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--epoch', type=int, help='epoch number', default=5)
     argparser.add_argument('--lr', type=float, help='learning rate', default=0.001)
-    argparser.add_argument('--batchsz', type=int, help='total batchsz for traindb', default=32)
+    argparser.add_argument('--batchsz', type=int, help='total batchsz for traindb', default=16)
     argparser.add_argument('--size', type=int, default=1250)
     argparser.add_argument('--path_data', type=str, default='/tf/training_dataset/')
     argparser.add_argument('--path_indices', type=str, default='./data_indices/')
